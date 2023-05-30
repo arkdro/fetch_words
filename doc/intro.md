41d984dc6d335c6ed23f74091bf2fb8fea10bda9
Arkadii Drovosekov <arkdro@protonmail.com>
2023-05-30 19:14:08 +0200

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to fetch_words
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/fetch_words/core_test.clj
@@ -0,0 +1,7 @@
+(ns fetch-words.core-test
+  (:require [clojure.test :refer :all]
+            [fetch-words.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
