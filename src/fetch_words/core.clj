85e17adb27d3ff3b78277d752ea9fc770f0692b0
Arkadii Drovosekov <arkdro@protonmail.com>
2023-05-30 19:14:08 +0200

    handle command line arguments

--- src/fetch_words/core.clj
+++ src/fetch_words/core.clj
@@ -1,6 +1,47 @@
-(ns fetch-words.core)
+(ns fetch-words.core
+  (:require [clojure.tools.cli :refer [parse-opts]]
+            ;; [clojure.tools.trace :as trace]
+            [fetch-words.process]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def cli-options
+  ;; An option with a required argument
+  [["-w" "--wordlist WORDLIST" "a file with the list of words"
+    :validate [#(and
+                 (some? %)
+                 (string? %))
+               "Must be non-empty string"]]
+   ["-o" "--outdir OUTDIR" "output dir"
+    :validate [#(and
+                 (some? %)
+                 (string? %))
+               "Must be non-empty string"]]
+   ["-l" "--levels N" "create N levels of output directories. Default: round(log10(wordlist size))"
+    :validate [#(and
+                 (some? %)
+                 (string? %))
+               "Must be non-empty string"]]
+   ;; A boolean option defaulting to nil
+   ["-h" "--help"]])
+
+(defn activate_trace
+  []
+  ;; (trace/trace-ns 'fetch-words.process)
+  )
+
+(defn -main
+  "Parse command line arguments and run the generator"
+  [& args]
+  (activate_trace)
+  (let [opts (parse-opts args cli-options)
+        var_arguments (get opts :arguments)
+        options (get opts :options)
+        wordlist (get options :wordlist)
+        outdir (get options :outdir)
+        levels (get options :levels)
+        errors (get opts :errors)
+        help (get-in opts [:options :help])]
+    (cond
+      help (println (get opts :summary))
+      errors (println errors)
+      :default (fetch-words.process/process_word_list wordlist outdir levels var_arguments)))
+  (System/exit 0))
