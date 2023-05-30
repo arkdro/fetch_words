ba5587cd52fed115d6299d686b4a2cd0eb9c2876
Arkadii Drovosekov <arkdro@protonmail.com>
2023-05-30 19:14:09 +0200

    move similar testings into a common test

--- test/fetch_words/process_test.clj
+++ test/fetch_words/process_test.clj
@@ -9,14 +9,12 @@
   [name]
   (slurp (io/file (io/resource name))))
 
-(deftest extract_payload_test_simple
+(deftest extract_payload_test
   (testing "extract payload data from a string with values"
     (let [text "1,'xx',2,3,4,'','ina,bc&nbsp;d.e.fg\"jpg\"','h',5,6,''"
           actual (extract_payload text)
           expected "ina,bc&nbsp;d.e.fg\"jpg\""]
-      (is (= actual expected)))))
-
-(deftest extract_payload_test_2
+      (is (= actual expected))))
   (testing "extract payload data from a string with values"
     (let [text (get_resource "one_line_values.txt")
           expected (get_resource "one_line_values_expected")
